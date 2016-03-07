508c3d03155d299b18c9a00655f2b54b02f5a6eb
Christopher Hewett <chewett@hotmail.co.uk>
2016-03-07 22:14:43 +0000

    Added a basic start to the JSUglify2 library

---
+++ tests/Chewett/UglifyJS2/JSUglify2Test.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Chewett\UglifyJS2;
+
+class JSUglify2Test extends \PHPUnit_Framework_TestCase
+{
+
+    public static $buildDir = __DIR__ . '/../../../build/output/';
+
+    public function setUp() {
+        if(!is_dir(self::$buildDir)) {
+            mkdir(self::$buildDir);
+        }
+    }
+
+    public function testCheckVersionWorks() {
+        $ug = new JSUglify2();
+        $this->assertTrue($ug->checkUglifyJsExists());
+    }
+
+    /**
+     * @expectedException \Chewett\UglifyJS2\UglifyJs2Exception
+     */
+    public function testFileNotReadable() {
+        $ug = new JSUglify2();
+        $ug->uglify(["not_a_file"], "output.js");
+    }
+
+    public function testRunningOnJquery() {
+        $ug = new JSUglify2();
+        $output = $ug->uglify([__DIR__ . '/../../../vendor/components/jquery/jquery.js'], self::$buildDir . 'jquery.min.js');
+        $this->assertNotNull($output);
+    }
+
+    public function testRunningOnTwitterBootstrap() {
+        $ug = new JSUglify2();
+        $twitterBootstrapDir = __DIR__ . '/../../../vendor/twbs/bootstrap/js/';
+        $output = $ug->uglify([
+            $twitterBootstrapDir . "affix.js",
+            $twitterBootstrapDir . "alert.js",
+            $twitterBootstrapDir . "button.js",
+            $twitterBootstrapDir . "collapse.js",
+            $twitterBootstrapDir . "dropdown.js",
+            $twitterBootstrapDir . "modal.js",
+            $twitterBootstrapDir . "popover.js",
+            $twitterBootstrapDir . "scrollspy.js",
+            $twitterBootstrapDir . "tab.js",
+            $twitterBootstrapDir . "tooltip.js",
+            $twitterBootstrapDir . "transition.js"
+        ], self::$buildDir . 'bootstrap.min.js');
+        $this->assertNotNull($output);
+    }
+
+}
