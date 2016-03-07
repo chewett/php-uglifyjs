b2d13242e0baffd02d04523c1fcc8c49aad99aa9
Christopher Hewett <chewett@hotmail.co.uk>
2016-03-07 21:04:00 +0000

    Added base exception to encompass all exceptions thrown in this library

---
+++ src/Chewett/UglifyJS2/UglifyJs2Exception.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Chewett\UglifyJS2;
+
+/**
+ * Class UglifyJs2Exception
+ * Basic class to encapsulate all the exceptions thrown by this library
+ * @package Chewett\UglifyJS2
+ * @author Christopher Hewett <chewett@hotmail.co.uk>
+ */
+class UglifyJs2Exception extends \Exception
+{
+
+}
