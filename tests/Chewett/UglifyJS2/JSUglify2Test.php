c7119347e296a8f6674fd38882fc4aed52b704e2
Christopher Hewett <chewett@hotmail.co.uk>
2016-03-09 21:53:23 +0000

    Updated composer.json and added some comments to tests

--- tests/Chewett/UglifyJS2/JSUglify2Test.php
+++ tests/Chewett/UglifyJS2/JSUglify2Test.php
@@ -1,7 +1,14 @@
 <?php
 
-namespace Chewett\UglifyJS2;
+namespace Chewett\UglifyJS22asdasd;
 
+use Chewett\UglifyJS2\JSUglify2;
+
+/**
+ * Class JSUglify2Test
+ * @package Chewett\UglifyJS22asdasd
+ * @author Christopher Hewett <chewett@hotmail.co.uk>
+ */
 class JSUglify2Test extends \PHPUnit_Framework_TestCase
 {
 
@@ -13,12 +20,16 @@ class JSUglify2Test extends \PHPUnit_Framework_TestCase
         }
     }
 
+    /**
+     * Very basic check to see if the jsuglify version flag works
+     */
     public function testCheckVersionWorks() {
         $ug = new JSUglify2();
-        $this->assertTrue($ug->checkUglifyJsExists());
+        $this->assertTrue($ug->checkUglifyJsExists(), "Test to run jsuglify failed, is it installed?");
     }
 
     /**
+     * Tests to see if it fails with the expected exception on a non file
      * @expectedException \Chewett\UglifyJS2\UglifyJs2Exception
      */
     public function testFileNotReadable() {
@@ -26,12 +37,18 @@ class JSUglify2Test extends \PHPUnit_Framework_TestCase
         $ug->uglify(["not_a_file"], "output.js");
     }
 
+    /**
+     * Tests to see if running the file on a single file works
+     */
     public function testRunningOnJquery() {
         $ug = new JSUglify2();
         $output = $ug->uglify([__DIR__ . '/../../../vendor/components/jquery/jquery.js'], self::$buildDir . 'jquery.min.js');
         $this->assertNotNull($output);
     }
 
+    /**
+     * Tests to see if minifying multiple files throws an error
+     */
     public function testRunningOnTwitterBootstrap() {
         $ug = new JSUglify2();
         $twitterBootstrapDir = __DIR__ . '/../../../vendor/twbs/bootstrap/js/';
@@ -51,6 +68,9 @@ class JSUglify2Test extends \PHPUnit_Framework_TestCase
         $this->assertNotNull($output);
     }
 
+    /**
+     * Tests to see if mangling jquery will not throw an error
+     */
     public function testRunningOnJqueryWithMangle() {
         $ug = new JSUglify2();
         $output = $ug->uglify(
